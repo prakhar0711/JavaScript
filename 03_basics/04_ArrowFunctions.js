da27408dcf088fba9046808eb1a2e19c656a59be
Prakhar Pratyush <prakhar.pratyush8010@gmail.com>
2024-01-30 19:42:45 +0530

    arrow

--- 03_basics/04_ArrowFunctions.js
+++ 03_basics/04_ArrowFunctions.js
@@ -1,5 +1,13 @@
-const user={
-    name:"sachin",
-    age:23
-}
+const user = {
+  username: "tushar",
+  age: 23,
+  welcomeMessage: function () {
+    console.log(`${this.username} ,welcome to website`);
+    console.log(this);
+  },
+};
+
+user.welcomeMessage();
+user.username = "sam";
+user.welcomeMessage();
 
