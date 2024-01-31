faf64342b187c301959e30240f62ed06a0695133
Prakhar Pratyush <prakhar.pratyush8010@gmail.com>
2024-01-31 13:25:07 +0530

    while loop

---
+++ 05_Iterations/02_while_doWhile.js
@@ -0,0 +1,19 @@
+// while (true){
+//     expression
+// }
+
+let index = 0;
+while (index <= 10) {
+  console.log(`${index}`);
+  index += 2;
+}
+
+// do {
+//   expressions;
+// } while (condition);
+// first do loop is executed then check the condition
+let score = 6;
+do {
+  console.log(`${score}`);
+  score++;
+} while (score <= 5);
