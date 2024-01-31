38f1bf631227fabae178e953fd60555903ebcfaa
Prakhar Pratyush <prakhar.pratyush8010@gmail.com>
2024-01-31 14:44:49 +0530

    filter

---
+++ 05_Iterations/06_filter.js
@@ -0,0 +1,30 @@
+// const coding = ["js", "C++", "java"];
+
+// const values = coding.forEach((item) => {
+//   console.log(item);//returns js C++ java
+// });
+// console.log(values); //returns undefined
+
+// const values = coding.forEach((item) => {
+//   //console.log(item);
+//   return item; //returns js C++ java
+// });
+// console.log(values); //returns undefined
+//forEach never returns anything
+// *************************************************************
+//filter returns something
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const values = myNums.filter((num) => {
+//   return num > 5;
+// });
+// console.log(values);
+
+
+//returning with forEach loop
+const newNums = [];
+myNums.forEach((num) => {
+  if (num > 5) {
+    newNums.push(num);
+  }
+});
+console.log(newNums);
