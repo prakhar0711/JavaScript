43b7c8e61d07bb6e9586921f30c1992f35e26972
Prakhar Pratyush <prakhar.pratyush8010@gmail.com>
2024-02-01 13:36:07 +0530

    colour_changer

--- 07_Basic-Projects/01_ColourChanger/colourchange.js
+++ 07_Basic-Projects/01_ColourChanger/colourchange.js
@@ -8,13 +8,13 @@ buttons.forEach((button) => {
     // console.log(event);
     // console.log(event.target);
     if (event.target.id === "grey") {
-      body.style.backgroundColor = "grey";
+      body.style.backgroundColor = event.target.id;
     } else if (event.target.id === "white") {
-      body.style.backgroundColor = "white";
-    } else if (event.target.id == "blue") {
-      body.style.backgroundColor = "blue";
-    } else if (event.target.id == "yellow") {
-      body.style.backgroundColor = "yellow";
+      body.style.backgroundColor = event.target.id;
+    } else if (event.target.id === "blue") {
+      body.style.backgroundColor = event.target.id;
+    } else if (event.target.id === "yellow") {
+      body.style.backgroundColor = event.target.id;
     }
   });
 });
